d1c3c80f5b323e834775028e4a1146ce943f4ffc
haohao Chang <9203700@gmail.com>
2017-06-24 02:41:46 +0800

    controllers

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,48 @@
 class ActivitiesController < ApplicationController
+    before_action :set_activity, :only=>[:show,:edit,:destroy]
+    before_action :authenticate_admin!, :only=>[:new,:create,:edit,:update,:destroy]
+    def index
+        @activities=Activity.all
+    end
+    def new
+        @activity=Activity.new
+    end
+    def create
+        @activity=Activity.new(activity_params)
+        @activity.user_id=current_admin.id
+        if @activity.save
+            #flash[:notice]="成功創建一筆團購"
+            redirect_to activities_path,:method=>:get
+        else
+            #flash[:alert]="欄位輸入錯誤"
+            render :action=>:new
+        end
+    end
+    def show
+       save_activityID(@activity)
+    end
+    def edit
+        if current_user.id!=@activity.user_id
+            #flash[:alert]="無法編輯他人團購"
+            redirect_to activities_path,:method=>:get
+        end
+    end
+    def update
+        if @activity.update(activity_params)
+            #flash[:notice]="成功更新一筆團購"
+            redirect_to activities_path,:method=>:get
+        else
+            #flash[:alert]="欄位輸入錯誤"
+            render :action=>:edit
+        end
+    end
+    def destroy
+    end
+private
+    def set_activity
+        @activity = Activity.find(params[:id])
+    end
+    def activity_params
+        params.require(:activity).permit(:title,:shop_id,:cuttime,:picktime,:estquantity,:place)
+    end
 end
---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,47 @@
+class OrdersController < ApplicationController
+    before_action :set_order,:only=>[:show,:edit,:update,:delete]
+    before_action :authenticate_admin!    
+    def new
+        @order=Order.new
+    end
+    def create
+        @order=Order.new(order_params)
+        @order.user_id=current_admin.id
+        @order.activity_id = get_activityID
+        if @order.save
+            @activity=Activity.find_by(:id=>get_activityID)
+            @activity.increment(:cur_quantity)
+            @activity.save
+            #flash[:notice]="成功訂購一杯飲料"
+            redirect_to new_order_path,:method=>:get
+        else
+            #flash[:alert]="欄位填寫錯誤，請重新填寫"
+            render :action=>:new
+        end
+    end
+    def show
+    end
+    def edit
+    end
+    def update
+        if @order.update(order_params)
+            #flash[:notice]=""
+            redirect_to new_order_path,:method=>:get
+        else
+            #flash[:notice]=""
+            render :action=>:edit
+        end
+    end
+    def destroy
+        @order.destroy
+        #@flash[:notice]=""
+        redirect_to new_order_path,:method=>:get
+    end
+private
+    def order_params
+        params.require(:order).permit(:item,:sugar_level,:ice_level,:price)
+    end
+    def set_order
+        @order=Order.find(params[:id])
+    end
+end
