6b5ad3a19bfaf172c610011b4d633cddd206889c
haohao Chang <9203700@gmail.com>
2017-06-24 02:44:44 +0800

    new resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   resources :manages
-  root to: 'manages#index'
+  resources :activities
+  resources :orders
+  root to: 'activities#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
