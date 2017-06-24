2e99f9835d61cccede9caf466aa786ff64c15b32
haohao Chang <9203700@gmail.com>
2017-06-24 13:21:28 +0800

    update route list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
   resources :manages
-  resources :activities
+  resources :activities do
+      collection do
+          get :youractivity
+      end
+  end
   resources :orders
   root to: 'activities#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
